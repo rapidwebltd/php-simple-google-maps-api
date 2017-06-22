0dc266cdb3665b1925578c8d4a1e65fe934d1caf
Jordan Hall <jordan.hall@rapidweb.biz>
2017-06-22 09:49:24 +0100

    Add check for results before creating LatLong object

--- src/Objects/SimpleGoogleMaps.php
+++ src/Objects/SimpleGoogleMaps.php
@@ -36,6 +36,11 @@ class SimpleGoogleMaps
     if($format == "json"){
         $results = json_decode($getResults->getBody());
     }
+
+    if(!isset($results) || !$results || !$results->results || !isset($results->results[0])) {
+      return null;
+    }
+
     $latLong = new LatLong($results->results[0]->geometry->location->lat,$results->results[0]->geometry->location->lng);
 
     return $latLong;
