9e9d19b68d0be959859cbc4a141595760dc58023
Jordan Hall <jordan.hall@rapidweb.biz>
2017-07-06 09:38:33 +0100

    Minor improvements to example file

--- src/example.php
+++ src/example.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 use RapidWeb\SimpleGoogleMaps\Factories\SimpleGoogleMapsFactory;
 
@@ -8,7 +8,6 @@ $addressline2 = "holyrood palace, Canongate, Edinburgh EH8 8DX UK";
 
 $simpleGoogleMaps = SimpleGoogleMapsFactory::getByClientNameAndCryptKey("[CLIENTNAME]","[CRYPTKEY]");
 $simpleGoogleMaps = SimpleGoogleMapsFactory::getByKey("[APIKEY]");
-
 
 $homeCoords = $simpleGoogleMaps->getByAddress($addressline);
 $toCoords = $simpleGoogleMaps->getByAddress($addressline2);
